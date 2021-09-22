daa65864bc7a7d95ce4604617449ca2e1d6284f9
maciek <maciek.pwns@gmail.com>
2021-09-22 15:11:20 +0200

    ADD: update to 3.0

--- utilities/files.py
+++ utilities/files.py
@@ -1,7 +1,5 @@
 import os
 import datetime
-import pandas
-
 
 def get_folders_files(path, wp=True):
     """
@@ -52,23 +50,3 @@ def get_files(path, prefix, extension, wp=True):
 def make_folder(path):
     if not os.path.exists(path):
         os.mkdir(path)
-
-def csv_merge(path, filename):
-    """
-    merges all csvs in the directory without unnecessary headers, unnamed
-    columns
-    """
-
-    now = datetime.datetime.now()
-    day = str(now.day) + "-" + str(now.month) + "-" + str(now.year)
-    hour = str(now.hour) + "_" + str(now.minute) + "_" + str(now.second)
-
-    all_files = get_files(path, '', 'csv', wp=True)[2]
-
-    dataframes = [pd.read_csv(i) for i in all_files]
-
-    data = pd.concat(dataframes, ignore_index=True)
-    headers = list(data)
-    data = data[headers[:-1]]
-
-    data.to_csv('{0}{1}{2}_{3}_{4}.csv'.format(path, os.sep, filename, day, hour))
